4d7e7211bf6eb2a434c7329fadcea4bdae6b9b09
Lucas Ferreira <lbrandof@gmail.com>
2021-04-30 14:55:35 -0300

    add a report last 7 days

--- pages/reports.js
+++ pages/reports.js
@@ -1,13 +1,58 @@
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { Button, InputGroup, Form } from 'react-bootstrap'
+import { format } from 'date-fns'
 import styles from '../styles/Reports.module.css'
 import Cabecalho  from '../components/cabecalho'
 import Rodape from '../components/rodape'
 
+
 export default function Reports() {
+    const [lastSales, setLastSales] = useState([])
+
+
+
+    async function handlelastSales() {
+        try {
+            const response = await api.get('customers')
+            setLastSales(response.data)  
+        } catch {
+
+        }
+        
+    }
+    
+    useEffect(() => {
+        handlelastSales()
+    },[])
     return (
         <div className={styles.container}>
             <Cabecalho />
             <h2>Relatórios</h2>
-            <h3>Em breve</h3>
+
+            <h4>Vendas dos últimos 7 dias</h4>
+            
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nome Cliente</th>
+                        <th>Produto</th>
+                        <th>Valor</th>
+                        <th>Cradastrado em</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {lastSales.map( lastsale => {
+                        <tr key={lastsale.id_sale}>
+                            <td>lastsale.name_customer</td>
+                            <td>lastsale.name_product</td>
+                            <td>lastsale.price_product</td>
+                            <td>lastsale.name_customer</td>
+                        </tr>
+                        })
+                    }
+                </tbody>
+            </Table>
             <Rodape />
         </div>    
     )
