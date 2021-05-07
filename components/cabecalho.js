d3306fbcfbddf4aa1f4f4af90650e4055f633ba5
Lucas Ferreira <lbrandof@gmail.com>
2021-05-06 21:59:51 -0300

    Add more styles on header

--- components/cabecalho.js
+++ components/cabecalho.js
@@ -1,16 +1,26 @@
 import styles from '../styles/Cabecalho.module.css'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export default function Cabecalho() {
+  const [ session, loading ] = useSession()
+
     return ( 
           <div className={styles.cabecalho}>
             <div className={styles.logo}>
-              Logo
+              <img src="/static/adama-logotipo.png" width={32} height={32} />
             </div>  
             <div className={styles.title}>
               Sistemas de Gerenciamento de Vendas
             </div>
             <div className={styles.avatar}>
-              Avatar
+              { session ? (
+                <img src="/static/avatar-naiara.png" width={32} height={32} />
+               ) : (
+                <img src="/static/avatar-nobody.png" width={32} height={32} /> 
+               )
+               } 
+              
+              
             </div>
           </div>
     )
